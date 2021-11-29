8bf8c77343f2c8671bbb3799a83ad1ae1ba32716
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2021-11-29 17:44:10 +0000

    Added custom presence status message. Closes #11

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: 'ready',
   once: true,
-  async execute() {
+  async execute(client) {
     console.log('Ready!')
+    client.user.setPresence({ activities: [{ name: `discord.js tutorials.`, type: 'WATCHING' }], status: 'online' });
   },
 };
