42d84282f061292786dfda3d887d74b5441f1d8c
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2022-01-19 15:49:37 +0000

    Minor Updates to Setup Command

--- commands/setup.js
+++ commands/setup.js
@@ -13,8 +13,7 @@ module.exports = {
         .addChannelOption(option =>
           option
             .setName('channel')
-            .setDescription('Select a channel to create brewing threads in')
-            .setRequired(true))
+            .setDescription('Select a channel to create brewing threads in'))
         .addBooleanOption(option =>
           option
             .setName('enabled')
@@ -27,8 +26,7 @@ module.exports = {
         .addChannelOption(option =>
           option
             .setName('channel')
-            .setDescription('Select a channel to send user wishlists')
-            .setRequired(true))
+            .setDescription('Select a channel to send user wishlists'))
         .addBooleanOption(option =>
           option
             .setName('enabled')
@@ -36,19 +34,27 @@ module.exports = {
     ),
 
   async execute(interaction) {
+
     // Validate user permissions
     if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
       await interaction.reply({ content: `You must be an administrator to use this command.`, ephemeral: true });
       return;
     }
 
+    // Variables
     let enabled, statusString, replyString, update;
     const command = interaction.options.getSubcommand();
     const channel = interaction.options.getChannel('channel');
     enabled = interaction.options.getBoolean('enabled');
+
     if (enabled == null) { enabled = true; }
     if (enabled) { statusString = 'enabled'; } else { statusString = 'disabled'; }
+    if (enabled && channel == null) {
+      await interaction.reply({ content: `To enable the ${command} command, you must select a channel using the ` + "`channel` option.", ephemeral: true });
+      return;
+    }
 
+    // Define database update and reply string
     if (command === 'brew') {
       update = { $set: { 'brew.channelID': channel.id, 'brew.enabled': enabled } };
       replyString = `The ${command} command is now ${statusString}.`;
@@ -59,6 +65,7 @@ module.exports = {
       if (enabled) { replyString = replyString + ` Wishlists will be sent to the ${channel} channel.`; }
     }
 
+    // Update database and send reply
     await guildProfile.findByIdAndUpdate(interaction.guild.id, update, { upsert: true });
     await interaction.reply({ content: replyString, ephemeral: true });
   },
